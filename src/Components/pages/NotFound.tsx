299566314e2eca4c4b59afb7df56768f767de6ee
ryosebach <ryo.freezia@gmail.com>
2019-05-08 09:15:13 +0900

    📝  feat: add route-conf and NotFound page

---
+++ src/Components/pages/NotFound.tsx
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { Helmet } from 'react-helmet';
+
+const renderHelmet = () => {
+ return(
+  <Helmet>
+    <title>Not Found</title>
+  </Helmet>
+ );
+};
+
+/**
+ * * Feed Component
+ */
+export const NotFound = () => {
+  return (
+    <div>
+      { renderHelmet() }
+      <h1>Page Not Found</h1>
+    </div>
+  );
+};
---
+++ src/Routes/routes.ts
@@ -0,0 +1,22 @@
+import { NotFound } from 'Components/pages/NotFound';
+
+const routes = [
+    // {
+    //     path: '/hoge',
+    //     component: HogePage,
+    //     routes: [
+    //         {
+    //           path: '/fuga',
+    //           exact: true,
+    //           component: FugaTemplate
+    //         }
+    //     ]
+    // },
+    {
+        path: '*',
+        component: NotFound
+    }
+  ];
+  
+  export default routes;
+  
